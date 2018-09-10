fae6157716f3d4bcdb2d0c7f6bab190dd53a867d
Joe <joemilsom@gmail.com>
2018-09-10 10:25:53 +0300

    refactor webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -21,7 +22,23 @@ module.exports = {
             hash: true, //adds unique hash to js each time. would be better linked to git commit hash
             filename: 'index.html',
             template : './src/index.html'
-    })
+    }),
+   //todo refactor with generic cdn
+    new CopyWebpackPlugin([ 
+        {
+         from : "./src/css",
+         to : path.resolve('./dist/css')
+        },
+        {
+         from : "./src/images",
+         to : path.resolve('./dist/images')
+        },
+        {
+         from : "src/shaders",
+         to : path.resolve('./dist/shaders')
+        }
+      ])
+
   ],
   resolve: {
     modules: [
